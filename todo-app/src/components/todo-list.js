b954094a5868ec7baf7741036a5efc01828e22c1
gooddha <gooddha@ya.ru>
2021-01-05 18:08:48 +0300

    start create components files

---
+++ todo-app/src/components/todo-list.js
@@ -0,0 +1,10 @@
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React JS</li>
+      <li>Drink Coffe</li>      
+    </ul>
+  );
+}
+
+export default TodoList;
--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,1 +1,39 @@
-console.log('Hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './components/todo-list'
+
+const items = ['Learn React JS', 'Buld Awesome App']
+
+
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+}
+
+const SearchPanel = () => {
+    const searchText = 'Type here to search';
+    const searchStyle = {
+      fontSize: '25px'
+    };
+    return <input type="text" style={searchStyle} placeholder={searchText} />;
+}
+
+const App = () => {
+
+  const isLoggedIn = true;
+  const loginBox = <span>Log in please</span>;
+  const welcomeBox = <span>Welcome back</span>
+
+  return (
+    <div>
+      { isLoggedIn ? welcomeBox : loginBox }
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+}
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<App /> , root);
