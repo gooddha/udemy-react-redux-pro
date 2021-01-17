c93186f28d2b050fa8b9939506aab707825b1edc
Andrey Adamovich <gooddha@ya.ru>
2021-01-17 19:33:01 +0300

    Made custom events

--- todo-app/src/components/app/app.js
+++ todo-app/src/components/app/app.js
@@ -8,7 +8,7 @@ import ItemStatusFilter from '../item-status-filter/';
 import './app.css';
 
 const App = () => {
-    
+
   const todoData = [
     { label: 'Drink Coffe', important: false, id: 1 },
     { label: 'Make Awesome App', important: true, id: 2 },
@@ -21,13 +21,13 @@ const App = () => {
 
   return (
     <div className="todo-app">
-      { isLoggedIn ? welcomeBox : loginBox }
-      <AppHeader toDo={2} done={3}/>
+      {isLoggedIn ? welcomeBox : loginBox}
+      <AppHeader toDo={2} done={3} />
       <div className="top-panel d-flex">
         <SearchPanel />
         <ItemStatusFilter />
       </div>
-      <TodoList todos = { todoData  } />
+      <TodoList todos={todoData} onDelete={(id) => console.log(id)} />
     </div>
   );
 }
--- todo-app/src/components/todo-list/todo-list.js
+++ todo-app/src/components/todo-list/todo-list.js
@@ -2,19 +2,19 @@
 import TodoListItem from '../todo-list-item/';
 import './todo-list.css'
 
-const TodoList = ( { todos } ) => {
+const TodoList = ({ todos, onDelete }) => {
 
-  const elements = todos.map( item => {
+  const elements = todos.map(item => {
     // return <li key={item.id}><TodoListItem label={item.label} important={item.important} /></li>      
     // return <li key={item.id}><TodoListItem { ...item } /></li>      
-    
+
     const { id, ...itemProps } = item;
-    return <li key={id} className="list-group-item"><TodoListItem { ...itemProps } /></li>      
+    return <li key={id} className="list-group-item"><TodoListItem {...itemProps} onDelete={() => onDelete(id)} /></li>
   })
 
   return (
     <ul className="list-group todo-list">
-      { elements }
+      {elements}
     </ul>
   );
 }
