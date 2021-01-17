afa06c5b75dbc2d4d047bf45c4617f16cce2cc13
Andrey Adamovich <gooddha@ya.ru>
2021-01-17 19:57:28 +0300

    Done todo list item deleting

--- todo-app/src/components/app/app.js
+++ todo-app/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header/';
 import SearchPanel from '../search-panel/';
@@ -7,30 +7,37 @@ import ItemStatusFilter from '../item-status-filter/';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    { label: 'Drink Coffe', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+  state = {
+    todoData: [
+      { label: 'Drink Coffe', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  };
 
-  const isLoggedIn = true;
-  const loginBox = <span>Log in please</span>;
-  const welcomeBox = <span>Welcome back</span>
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex(el => el.id === id);
 
-  return (
-    <div className="todo-app">
-      {isLoggedIn ? welcomeBox : loginBox}
-      <AppHeader toDo={2} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+      return {
+        todoData: [...todoData.slice(0, idx), ...todoData.slice(idx + 1)]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={2} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList todos={this.state.todoData} onDelete={this.deleteItem} />
       </div>
-      <TodoList todos={todoData} onDelete={(id) => console.log(id)} />
-    </div>
-  );
+    );
+  }
+
 }
-
-
-export default App;
