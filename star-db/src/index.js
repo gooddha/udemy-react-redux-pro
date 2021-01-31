03c2cd929bb1a42f70d7e30434662596a2193fd0
Andrey Adamovich <gooddha@ya.ru>
2021-01-31 17:11:06 +0300

    Start second project. First commit

---
+++ star-db/src/index.js
@@ -0,0 +1,12 @@
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+
+  return body;
+};
+
+getResource('https://swapi.dev/api/people/1').then(body => {
+  console.log(body)
+}).catch(err => {
+  console.error('Couldnt catch ', err);
+});
