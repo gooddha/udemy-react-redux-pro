157c34538addb3bb1fca4adbec744a401a41f3d9
Andrey Adamovich <gooddha@ya.ru>
2021-02-14 17:59:42 +0300

    done api transformation lesson

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,20 +1,6 @@
 import ReactDOM from 'react-dom';
 
 import App from './components/app/';
-import SwapiService from './services/swapi-service';
-
-
-const swapi = new SwapiService();
-
-swapi.getAllPeople().then(people => {
-  people.forEach(p => {
-    console.log(p.name);
-  })
-})
-
-swapi.getPerson(3).then(p => {
-  console.log(p.name);
-})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
