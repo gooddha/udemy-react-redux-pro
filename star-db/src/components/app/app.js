222276d2cd536d054ba1c8b3bb1295d9ba520227
gooddha <gooddha@ya.ru>
2021-02-21 18:49:49 +0300

    done component error handling

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Header from '../header/';
 import RandomPlanet from '../random-planet/';
 import PeoplePage from '../people-page';
+import ErrorButton from '../error-button';
+import ErrorIndicator from '../error-indicator';
+
 
 import './app.css';
 
@@ -14,17 +17,46 @@ export default class App extends React.Component {
   }
 
   componentDidCatch() {
+    console.log('ComponentDidCatch()')
     this.setState({
       hasError: true
     });
   }
 
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };
+
  
   render() {
+
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet/> :
+      null;
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
+
+
     return (
       <div className="app">
         <Header />
-        <RandomPlanet />
+        {planet}
+
+        <div className="row mb2 button-row">
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+          </button>
+          <ErrorButton />
+        </div>
+
         <PeoplePage />          
         <PeoplePage />          
         <PeoplePage />          
--- star-db/src/components/people-page/people-page.js
+++ star-db/src/components/people-page/people-page.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ItemList from '../item-list/';
 import PersonDetails from '../person-details/';
 import './people-page.css';
+import ErrorIndicator from '../error-indicator';
 
 
 
 export default class PeoplePage extends React.Component {
   
   state = {
-    selectedPerson: 3
+    selectedPerson: 3,
+    hasError: false
   }
   
   onItemSelected = (id) => {
@@ -17,7 +19,18 @@ export default class PeoplePage extends React.Component {
     })
   }
 
+  componentDidCatch() {
+    this.setState({
+      hasError: true
+    })
+  }
+
   render() {
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
+
     return(
       <div className="row mb2">
         <div className="col-md-6">
