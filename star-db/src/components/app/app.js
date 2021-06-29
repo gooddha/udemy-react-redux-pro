7758a83a944a91455e8ef87f5ba36b36e0b45d8e
Andrey Adamovich <gooddha@ya.ru>
2021-06-29 20:40:33 +0300

    done hoc composition lesson

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -44,16 +44,9 @@ export default class App extends React.Component {
           <PlanetDetails itemId={5} />
           <StarshipDetails itemId={9} />
 
-          <PersonList  >
-            {({ name }) => <span>{name}</span>}
-          </PersonList>
-          <StarshipList  >
-            {({ name }) => <span>{name}</span>}
-          </StarshipList>
-          <PlanetList  >
-            {({ name }) => <span>{name}</span>}
-          </PlanetList>
-
+          <PersonList />
+          <StarshipList />
+          <PlanetList />
 
         </div>
       </ErrorBoundry>
